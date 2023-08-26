e5415b51d8150a18636bc46f86fad36eb250086b
Muslima Akbarova <68753473+BonnieCoder@users.noreply.github.com>
2023-08-26 23:24:45 +0500

    Update Bird.java

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -5,4 +5,14 @@ public class Bird extends Animal {
     super.color = "blue";
     super.numberOfPaws = 2;
     super.hasFur = false;
+  }
+  public String getDescription() {
+    String text = "";
+    if(hasFur==true) {
+      text = "This animal is mostly "+color+". It has "+numberOfPaws+" paws and no fur. Moreover, it has 2 wings and can fly.";
+    } else {
+      text = "This animal is mostly "+color+". It has "+numberOfPaws+" paws and a fur. Moreover, it has 2 wings and can fly.";
+    }
+    return text;
+  }
 }
